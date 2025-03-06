5dc5f793f1e15fc0608fdc4d02777d9d547ec6fc
shingosan1123 <shingosan.1123@gmail.com>
2025-03-06 18:58:05 +0900

    MovieGenreViewModel.csにコメントアウトを記述しました

--- Models/MovieGenreViewModel.cs
+++ Models/MovieGenreViewModel.cs
@@ -5,8 +5,12 @@ namespace MvcMovie.Models;
 
 public class MovieGenreViewModel
 {
+    // フィルタリングされた映画リスト
     public List<Movie>? Movies { get; set; }
+    // ドロップダウンリスト用のジャンルデータ 
     public SelectList? Genres { get; set; }
+    // 選択されたジャンル 
     public string? MovieGenre { get; set; }
+    // フォームの検索文字列 
     public string? SearchString { get; set; }
 }
